cf8a109be0e43c067f3a9b5579d5f7e5e0b114a7
TriAlley <1203732857@qq.com>
2020-04-13 21:35:40 +0800

    lex2

---
+++ lex.cpp
@@ -0,0 +1,253 @@
+#include "lex.h"
+#define dout cout
+/*暴露给外界的数据*/
+char symTB[29][15] = {
+"const","var","procedure","begin","end","odd","if","then","call","while","do","read","write",//0-12
+"+","-","*","/","=","#","<","<=",">",">=",":=",";",",",".","(",")"//13-28
+};//关键字与算符界符表
+Lexword word[word_max_num];
+int wordnum = 0;//标记文件中单词数量
+
+
+
+
+
+Lex::Lex () {}
+
+void Lex::setFileName (string filename) {
+	this->filename = "./" + filename;
+}
+//******************************************
+//判断是否为字母
+bool Lex::IsLetter (char letter) {
+	if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
+		return true;
+	else
+		return false;
+
+}
+//******************************************
+//判断是否为数字 
+bool Lex::IsDigit (char digit) {
+	if (digit >= '0' && digit <= '9')
+		return true;
+	else
+		return false;
+}
+//******************************************
+//查找关键字表，找到返回类别（位置+1）
+int Lex::Retract (char* te) {
+	for (int i = 0; i < 13; i++) {
+		if (strcmp (te, symTB[i]) == 0) {
+			return i + 1;
+		}
+	}
+	return -1;
+}
+//******************************************
+//判断是否为算符或界符，找到返回位置+1，否则返回-1
+int Lex::IsSoj (char soj) {
+
+	for (int i = 13; i < 29; i++) {
+		if (soj == symTB[i][0]) {
+			return i + 1;
+		}
+
+	}
+	return -1;
+}
+//******************************************
+//判断是否为算符或界符，找到返回位置+1，否则返回-1
+int Lex::IsSoj (char* soj) {
+
+	for (int i = 13; i < 29; i++) {
+		if (strcmp (soj, symTB[i]) == 0) {
+			return i + 1;
+		}
+
+	}
+	return -1;
+}
+//******************************************
+//从标识符表中查找标识符，如果没有则插入，最后返回指针（位置）
+int Lex::InsertId (char* te) {
+	for (int i = 0; i < id; i++) {
+		if (strcmp (te, idTB[i]) == 0) {
+			return i;
+		}
+	}
+	strcpy_s (idTB[id++], strlen (te) + 1, te);
+	return id - 1;
+
+}
+//******************************************
+//从数字表表中查找数字，如果没有则插入，最后返回指针（位置）
+int Lex::InsertNum (char* te) {
+	for (int i = 0; i < id; i++) {
+		if (strcmp (te, numTB[i]) == 0) {
+			return i;
+		}
+	}
+	strcpy_s (numTB[num++], strlen (te) + 1, te);
+	return num - 1;
+
+}
+//******************************************
+//将一个大写字符转换为小写字符
+char Lex::tran (char te) {
+	if (te >= 'A' && te <= 'Z') {
+		te = te + 32;
+		return te;
+	} else
+		return te;
+}
+//根据文件，循环处理一个字符串，并输出到word以及文件以及终端
+int Lex::getsym () {
+
+	if (!sourfile.is_open () || !destfile.is_open ()) {
+		cout << "错误！源文件未打开成功\n";
+		return -1;
+	}
+	int row = 1;
+	ch = ' ';
+	dout << left << setw (15) << "name" << left << setw (15) << "sym" << left << setw (15) << "value&addr" << left << setw (15) << "row" << "\n";
+	destfile << left << setw (15) << "name" << left << setw (15) << "sym" << left << setw (15) << "value&addr" << left << setw (15) << "row" << "\n";
+	while (ch != EOF) {
+		p = 0;
+		sourfile.get (ch);
+		while (ch == ' ' || ch == 10){//过滤空格与换行
+			if (ch == 10) {
+				row++;
+			}
+			ch = sourfile.get ();
+		}
+
+		if (ch == EOF) {
+			return 1;
+		}
+
+		if (IsLetter (ch)){//读到的是字母，则要么是关键字，要么是标识符
+			strToken[p++] = tran (ch);
+			sourfile.get (ch);
+			while (IsDigit (ch) || IsLetter (ch)){//如果是数字或字母，则继续读取
+				strToken[p++] = tran (ch);
+				sourfile.get (ch);
+			}
+			sourfile.seekg (-1, ios::cur);//回退一格
+			code = Retract (strToken);//查找关键字表
+			if (code == -1) {//没找到，插入标识符表
+				value = InsertId (strToken);
+				word[wordnum].name = strToken;
+				word[wordnum].sym = IDENT;
+				word[wordnum].row = row;
+				wordnum++;
+				dout << left << setw(15) << idTB[value] << left << setw(15) << IDENT << left << setw(15) << value << "\n";
+				destfile << left << setw (15) << idTB[value] << left << setw (15) << IDENT << left << setw (15) << value << left << setw (15) << row << "\n";
+			} else {
+				word[wordnum].name = strToken;
+				word[wordnum].sym = code;
+				word[wordnum].row = row;
+				wordnum++;
+				dout << left << setw(15) << symTB[code - 1] << left << setw(15) << code << left << setw(15) << other << "\n";
+				destfile << left << setw (15) << symTB[code - 1] << left << setw (15) << code << left << setw (15) << other << left << setw (15) << row << "\n";
+
+			}
+
+		} else if (IsDigit (ch)) {
+			strToken[p++] = ch;
+			sourfile.get (ch);
+			while (IsDigit (ch))//如果是数字，则继续读取
+			{
+				strToken[p++] = ch;
+				sourfile.get (ch);
+			}
+			sourfile.seekg (-1, ios::cur);//回退一格
+
+			value = InsertNum (strToken);
+			word[wordnum].name = strToken;
+			word[wordnum].sym = NUMBER;
+			word[wordnum].row = row;
+			wordnum++;
+			string temp (strToken);
+			int num = atoi (temp.c_str ());
+			cout << left << setw(15) << numTB[value] << left << setw(15) << NUMBER << left << setw(15) << value << "\n";
+			destfile << left << setw (15) << numTB[value] << left << setw (15) << NUMBER << left << setw (15) << value << left << setw (15) << row << "\n";
+
+		} else if ((code = IsSoj (ch)) != -1) {
+			strToken[p++] = ch;
+			if (ch == ':') {
+				sourfile.get (ch);
+				if (ch == '=') {
+					strToken[p++] = ch;
+					word[wordnum].name = strToken;
+					word[wordnum].sym = code;
+					word[wordnum].row = row;
+					wordnum++;
+					dout << left << setw(15) << strToken << left << setw(15) << code << left << setw(15) << other << "\n";
+					destfile << left << setw (15) << strToken << left << setw (15) << code << left << setw (15) << other << left << setw (15) << row << "\n";
+				} else {
+					sourfile.seekg (-1, ios::cur);//回退一格
+					printf ("error\n");
+					return -1;
+				}
+			} else if (ch == '>' || ch == '<') {
+				sourfile.get (ch);
+				if (ch == '=') {
+
+					strToken[p++] = ch;
+					code = IsSoj (strToken);
+					word[wordnum].name = strToken;
+					word[wordnum].sym = code;
+					word[wordnum].row = row;
+					wordnum++;
+					dout << left << setw(15) << strToken << left << setw(15) << code << left << setw(15) << other << "\n";
+					destfile << left << setw (15) << strToken << left << setw (15) << code << left << setw (15) << other << left << setw (15) << row << "\n";
+				} else {
+					sourfile.seekg (-1, ios::cur);//回退一格
+					word[wordnum].name = strToken;
+					word[wordnum].sym = code;
+					word[wordnum].row = row;
+					wordnum++;
+					dout << left << setw(15) << strToken << left << setw(15) << code << left << setw(15) << other << "\n";
+					destfile << left << setw (15) << strToken << left << setw (15) << code << left << setw (15) << other << left << setw (15) << row << "\n";
+				}
+			} else {
+				word[wordnum].name = strToken;
+				word[wordnum].sym = code;
+				word[wordnum].row = row;
+				wordnum++;
+				dout << left << setw(15) << strToken << left << setw(15) << code << left << setw(15) << other << "\n";
+				destfile << left << setw (15) << strToken << left << setw (15) << code << left << setw (15) << other << left << setw (15) << row << "\n";
+			}
+
+		} else {
+			printf ("error\n");
+			return -1;
+		}
+		ch = ' ';
+		memset (strToken, 0, sizeof (strToken));
+		//memset(x, 0, sizeof(x));
+
+	}
+	return 1;
+
+
+}
+//******************************************
+//词法分析入口，通过文件名对文件进行词法分析
+bool Lex::runAnalysis () {
+	bool flag = false;
+	sourfile.open (filename);//打开源文件进行读
+	string tempfilename = filename.substr (0, filename.length () - 4) + "_lex.txt";
+	destfile.open (tempfilename, ios::out);//新建文件进行写
+	if (!sourfile.is_open () || !destfile.is_open ()) {
+		cout << "未成功打开文件" << filename << endl;
+		return flag;
+	}
+	int flagint = getsym ();
+	sourfile.close ();
+	destfile.close ();
+	if (flagint == 1)
+		flag = true;
+	return flag;
+}
