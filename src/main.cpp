b9e8b60ea19b8ec6c73177bc1c57c4729539c10d
LG <1203732857@qq.com>
2020-05-04 20:03:34 +0800

    fixed

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 
 #include "parser.h"
-// vm t(target);
+#include "vm.h"
 void currentdir() {
 	char szBuf[128];
 	char szPath[128];
@@ -26,20 +26,6 @@ void currentdir() {
 }
 void tests() {
 	currentdir();
-	/*
-	std::filesystem::path str("../TestCodeV2");
-	if (!std::filesystem::exists(str)) {
-		throw std::exception("TestCodeV2 does not exist.");
-	}
-	std::filesystem::directory_entry entry(str);						 //文件入口
-	if (entry.status().type() == std::filesystem::file_type::directory)	 //这里用了C++11的强枚举类型
-		std::cout << "该路径是一个目录" << std::endl;
-
-	std::filesystem::directory_iterator list(str);	//文件入口容器
-	for (auto& it : list) {							//对于文件夹中每一个成员
-	}
-	std::cout << it.path().filename() << std::endl;  //通过文件入口（it）获取path对象，再得到path对象的文件名，将之输出
-	*/
 	std::vector<std::string>
 		inFilePaths{
 			"./TestCodeV2/PL0_code.in",
@@ -61,7 +47,20 @@ void tests() {
 	}
 }
 void run(int argc, const char* argv[]) {
-	//TODO
+	parser a("./TestCodeV2/PL0_code2.in");
+
+	std::cout << std::endl;
+	std::cout << "符号表中内容如下:" << std::endl;
+	a.printTable();
+
+	std::cout << std::endl;
+	std::cout << "代码表中内容如下:" << std::endl;
+
+	a.printCode();
+
+	std::cout << "符号表(.sym)和目标代码(.pl0)都已输出在文件中." << std::endl;
+
+	vm t("./TestCodeV2/PL0_code2.in.pl0cache");
 }
 int main(int argc, const char* argv[]) {
 	if (argc > 0) {
